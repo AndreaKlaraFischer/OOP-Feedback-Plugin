785018fdfd058f49093f09aa167e0a87e4848297
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-09-16 18:50:37 +0200

    Testoutput for SubmitButton and several Tabs for ToolWindow
    Created Textarea and Submitbutton. The button has an ActionListener with outputs on the console with the text and a test output. Added tabs to the ToolWindow.

--- Plugin/src/main/java/gui/ToolWindowPlugin.java
+++ Plugin/src/main/java/gui/ToolWindowPlugin.java
@@ -1,23 +1,44 @@
 package gui;
 
 import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.ui.ComboBox;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import javax.swing.*;
 
-public class ToolWindowPlugin {
-    private JButton testButton;
+public class ToolWindowPlugin implements ActionListener{
+    private JButton submitRequestButton;
     private JPanel toolWindowContent;
+    //private JPanel contentAnswerScreen; //test
+   // private JPanel contentFAQScreen;
+    private JLabel title;
+    private JTextArea texti;
 
+
+    /*Each ToolWindow can contain multiple contents --> Das brauche ich für das Anzeigen im ersten Schritt für das zweistufige Interface,
+    * Und dann noch für das Anzeigen der Antworten
+    * Idee: Klicken auf das Plugin Icon geht der Posteingang auf und die Anfragen etc werden im ToolWindow angezeigt?*/
     public ToolWindowPlugin(ToolWindow toolwindow) {
-        testButton = new JButton("Test");
-
+       submitRequestButton.addActionListener(this::actionPerformed);
     }
 
+    //TODO: verschiedene Contents anzeigen lassen!
+    // switch?
+    // Sinnvoll: für jedes Panel eine eigene Klasse und ein .form file
+    // TODO: Herausfinden --> wie bekomme ich es dann in die ToolWindow Klasse?
+    //
     public JPanel getContent() {
-        return toolWindowContent;
+       return toolWindowContent;
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // ActionEvent anschauen!
+        System.out.println("Buttonklick funktioniert!");
+        System.out.println(e.getActionCommand());
+        System.out.println(texti.getText());
+        System.out.println("yay!");
+    }
 }
--- Plugin/src/main/java/gui/ToolWindowPluginFactory.java
+++ Plugin/src/main/java/gui/ToolWindowPluginFactory.java
@@ -8,12 +8,17 @@ import com.intellij.ui.content.*;
 
 public class ToolWindowPluginFactory implements ToolWindowFactory {
     // Tool window content gets created
+    // TODO: mehrere Tabs!
     //https://github.com/JetBrains/intellij-sdk-docs/blob/master/code_samples/tool_window/src/myToolWindow/MyToolWindowFactory.java
     @Override
     public void createToolWindowContent(Project project, ToolWindow toolWindow) {
         ToolWindowPlugin toolWindowPlugin = new ToolWindowPlugin(toolWindow) ;
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-        Content content = contentFactory.createContent(toolWindowPlugin.getContent(), "", false);
-        toolWindow.getContentManager().addContent(content);
+        Content contentRequest = contentFactory.createContent(toolWindowPlugin.getContent(), "Tutor fragen", false);
+        Content contentFAQ = contentFactory.createContent(toolWindowPlugin.getContent(), "FAQs", false);
+        Content contentAnswers = contentFactory.createContent(toolWindowPlugin.getContent(), "Antworten", false);
+        toolWindow.getContentManager().addContent(contentRequest);
+        toolWindow.getContentManager().addContent(contentFAQ);
+        toolWindow.getContentManager().addContent(contentAnswers);
     }
 }
