90feef3036e37652ea6e9acfec2a8c6bbb50792b
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-09-16 18:55:29 +0200

    Created class 'GitHubListener' for Communication from GitHub to the Plugin.

---
+++ Plugin/src/main/java/actions/GitHubListener.java
@@ -0,0 +1,22 @@
+package actions;
+// Hier die Github Kommunikation rein: Polling,...
+public class GitHubListener {
+ /*public void checkForNewAnswers() {
+     while (true) {
+         try {
+             incomingMessages.addAll(fullPoll());
+             System.out.println("waiting 6 seconds");
+             //perform this operation in a loop
+             Thread.sleep(6000);
+         } catch (InterruptedException e) {
+             // TODO Auto-generated catch block
+             e.printStackTrace();
+         } catch (Exception e) {
+             // TODO Auto-generated catch block
+             e.printStackTrace();
+         }
+     }
+
+ } */
+
+}
