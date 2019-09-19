b01f6b1a9b9bf0b8e0d501b4afecd6c01dd0c149
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-09-19 12:41:17 +0200

    Imported jgit to StudentRequestModel
    In this class, the repo should be cloned, the branch created and pushed (using jgit). Furthermore, the issue should be created (using GitHub API).

--- Plugin/src/main/java/requests/StudentRequestModel.java
+++ Plugin/src/main/java/requests/StudentRequestModel.java
@@ -1,10 +1,89 @@
 package requests;
 
+import com.intellij.openapi.project.Project;
+import org.eclipse.jgit.api.CheckoutCommand;
+import org.eclipse.jgit.api.CreateBranchCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
+import java.io.File;
+import java.io.IOException;
+
 public class StudentRequestModel implements RequestHandler {
-    @Override
-    public void addRequest(AddRequestArgument args) {
+    String repoUrl = "https://github.com/OOP-Feedback/OOP-Feedback";
+    File repoPath = new File("https://github.com/OOP-Feedback/OOP-Feedback");
+    File clonedRepoPath = new File("");
+
+    CreateBranchCommand createBranchCommand;
+    CheckoutCommand checkoutCommand;
+    Git git;
+
+    //clonedRepoPath = project.getBasePath();
+    //Konstruktor
+    public StudentRequestModel(Project project) {
 
     }
-    //hier Serverkommunikation implementieren
+
+    //TODO: Herausfinden: Wann wird das aufgerufen?
+    // --> Startklasse
+    public void cloneRepository() {
+        try {
+            Git.cloneRepository()
+                .setURI(repoUrl)
+                .setDirectory(clonedRepoPath)// #1
+                .call();
+
+        } catch (Exception e){
+
+        } finally {
+            //TODO: Ordner schließen
+        }
+    }
+
+    //Übernommen von diesem Codebeispiel
+    //https:stackoverflow.com/questions/33168230/jgit-create-new-local-branch-and-push-to-remote-branch-does-not-exist-on-remote
+    public void createAndPushBranch () {
+        String branchName ="";
+        try {
+            Repository repo = new FileRepositoryBuilder().readEnvironment().findGitDir(repoPath).build();
+            git = new Git(repo);
+
+            createBranchCommand = git.branchCreate();
+            checkoutCommand = git.checkout();
+        } catch (IOException e) {
+
+        }
+
+        try {
+            createBranchCommand.setName(branchName)
+                    .setUpstreamMode(CreateBranchCommand.SetupUpstreamMode.SET_UPSTREAM)
+                    .setStartPoint("origin/" + branchName)
+                    .setForce(true)
+                    .call();
+
+            checkoutCommand.setName(branchName);
+            checkoutCommand.call();
+        } catch (Exception e) {
+
+        }
+
+        // push to remote:
+
+    }
+
+    private void createIssue() {
+        //TODO: Use GitHub API here for creating issues
+        //Kopiert aus GitHub API:
+        //POST /repos/:owner/:repo/issues
+    }
+
+    //Diese Methode soll auf dem Button aufgerufen werden
+    @Override
+    public void addRequest(AddRequestArgument args) {
+        createAndPushBranch();
+        createIssue();
+    }
+
 
 }
