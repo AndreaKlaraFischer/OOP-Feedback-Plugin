4cafbb8933d7bb4fff7513ffb6751de308a9b224
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-09-20 10:28:45 +0200

    Refactored AnswerScreenClass
    Renamed the class, since the naming was ambigous.

--- Plugin/src/main/java/gui/AnswerDetailScreen.java
+++ Plugin/src/main/java/gui/AnswerDetailScreen.java
@@ -2,14 +2,15 @@ package gui;
 
 import javax.swing.*;
 
-public class AnswerScreen {
+public class AnswerDetailScreen {
+    //TODO: Dieser Screen soll nur bei Anklicken aufgehen!
     //TODO: neuen Tab öffnen zum Anzeigen des Codes
     //Textfeld, readonly: Antwort des Tutors wird angezeigt
     private JPanel answerScreenContent;
     private JButton button1;
     private JRadioButton radioButton1;
 
-    public AnswerScreen() {
+    public AnswerDetailScreen() {
 
     }
 
