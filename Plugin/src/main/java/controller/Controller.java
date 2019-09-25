dc399238c3238f99baa121f088b67b66f7e0065d
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-09-25 14:37:30 +0200

    Added test system output to Controller class in order to test
    Starterclass (Controller) works

--- Plugin/src/main/java/controller/Controller.java
+++ Plugin/src/main/java/controller/Controller.java
@@ -1,8 +1,18 @@
 package controller;
 
+import com.intellij.openapi.project.Project;
+import requests.StudentRequestModel;
+
 //zentrale Startklasse
 //beim Ausführen der Entwicklungsumgebung muss das Repo geklont werden
-
 public class Controller {
+    private final Project project;
+    private StudentRequestModel studentRequestModel;
+    public Controller(Project project) {
+        studentRequestModel = new StudentRequestModel(project);
+        this.project = project;
+        System.out.println("Startklasse funktioniert!");
+        //studentRequestModel.cloneRepo();
+    }
 
 }
