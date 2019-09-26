306fe2873e8eca762e586d5da8baedc23f79bcd2
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-09-26 19:50:45 +0200

    Implemented createAndPush Method
    Creating and pushing a branch with request id works

--- Plugin/src/main/java/requests/StudentRequestModel.java
+++ Plugin/src/main/java/requests/StudentRequestModel.java
@@ -4,41 +4,29 @@ import com.intellij.openapi.project.Project;
 import org.eclipse.jgit.api.CheckoutCommand;
 import org.eclipse.jgit.api.CreateBranchCommand;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.api.PushCommand;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.kohsuke.github.GHIssue;
 import org.kohsuke.github.GitHub;
 
 import java.io.File;
 import java.io.IOException;
-
 //GitHub API
 
 
 //This class handles the requests from Plugin to GitHub
 public class StudentRequestModel {
-    private static final String REPO_URL = "https://github.com/OOP-Feedback/OOP-Feedback";
+    private static final String REPO_URL = "https://github.com/OOP-Feedback/OOP-Feedback.git";
     private static final String CLONED_REPO_FOLDER ="/ClonedRepo";
-   // File repoPath = new File("https://github.com/OOP-Feedback/OOP-Feedback");
-    //TODO: Richtig befüllen!
-
-    //File clonedRepoPath = new File(;
-    Project project;
-
     private IDCreator idCreator;
+    private File REPO_PATH = new File("https://github.com/OOP-Feedback/OOP-Feedback.git");
+    private CreateBranchCommand createBranchCommand;
+    private CheckoutCommand checkoutCommand;
+    private File repoPath;
 
-    CreateBranchCommand createBranchCommand;
-    CheckoutCommand checkoutCommand;
-    Git git;
-
-    File repoPath;
-
-
-    //clonedRepoPath = project.getBasePath();
     //Konstruktor
     public StudentRequestModel(Project project) {
         this.idCreator = new IDCreator();
-        this.project = project;
         //TODO: zu speichernden Pfad festlegen!
         String clonedRepoPath = project.getBasePath() + CLONED_REPO_FOLDER;
         repoPath = new File(clonedRepoPath);
@@ -53,13 +41,13 @@ public class StudentRequestModel {
         try {
             System.out.println("Repo Methodenaufruf funktioniert!");
             Git.cloneRepository()
-               .setURI(REPO_URL)
-                    .setDirectory(repoPath)// #1
-                .call();
+                    .setURI(REPO_URL)
+                    .setCredentialsProvider( new UsernamePasswordCredentialsProvider( "OOP-Feedback", "FredFeedback1920" ) )
+                    .setDirectory(repoPath)
+                    .call();
             System.out.println("Repo wurde erfolgreich geklont");
-
         } catch (Exception e){
-            //?
+            System.out.println("Repo wurde nicht erfolgreich geklont" + e.toString());
         } finally {
             //TODO: Ordner schließen
         }
@@ -68,31 +56,31 @@ public class StudentRequestModel {
     //Übernommen von diesem Codebeispiel
     //https:stackoverflow.com/questions/33168230/jgit-create-new-local-branch-and-push-to-remote-branch-does-not-exist-on-remote
     public void createAndPushBranch () {
-        String branchName ="";
+        //Branchname: ID, das funktioniert schon mal
+        String branchName =idCreator.createRequestID();
+        System.out.println("Branchname" + branchName);
         try {
-            Repository repo = new FileRepositoryBuilder().readEnvironment().findGitDir(repoPath).build();
-            git = new Git(repo);
-
+           //Das geht nicht!
+            Git git = Git.open(repoPath);
             createBranchCommand = git.branchCreate();
+            createBranchCommand.setName(branchName);
+            createBranchCommand.call();
+            
             checkoutCommand = git.checkout();
-        } catch (IOException e) {
-
-        }
-
-        try {
-            createBranchCommand.setName(branchName)
-                    .setUpstreamMode(CreateBranchCommand.SetupUpstreamMode.SET_UPSTREAM)
-                    .setStartPoint("origin/" + branchName)
-                    .setForce(true)
-                    .call();
-
             checkoutCommand.setName(branchName);
             checkoutCommand.call();
-        } catch (Exception e) {
 
+            // push to remote:
+            PushCommand pushCommand = git.push();
+            pushCommand.setCredentialsProvider( new UsernamePasswordCredentialsProvider( "OOP-Feedback", "FredFeedback1920" ));
+            pushCommand.call();
+            System.out.println("puschi");
+
+        } catch (Exception e) {
+            System.out.println("zweite Exception branch create" +  e.toString());
         }
 
-        // push to remote:
+
 
     }
 
