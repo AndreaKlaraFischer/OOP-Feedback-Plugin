eb8ca719ac8a880bec177db86ab69421f20c55d3
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-09-26 17:20:21 +0200

    Create request ID
    Implemented method for creating a unique request id, concentated of a uuid and max address. Concenating and uuid work, getHardwareAddress() still in progress.

--- Plugin/src/main/java/controller/Controller.java
+++ Plugin/src/main/java/controller/Controller.java
@@ -2,18 +2,22 @@ package controller;
 
 import com.intellij.openapi.project.Project;
 import requests.StudentRequestModel;
+import requests.IDCreator;
 
 //zentrale Startklasse
 //beim Ausführen der Entwicklungsumgebung muss das Repo geklont werden
 public class Controller {
-    private final Project project;
+    private Project project;
     private StudentRequestModel studentRequestModel;
+    private IDCreator idCreator;
     public Controller(Project project) {
         studentRequestModel = new StudentRequestModel(project);
+        idCreator = new IDCreator();
         this.project = project;
         System.out.println("Startklasse funktioniert!");
         System.out.println(project.getBasePath());
         studentRequestModel.cloneRepo();
+        idCreator.createRequestID();
     }
 
 }
--- Plugin/src/main/java/requests/IDCreator.java
+++ Plugin/src/main/java/requests/IDCreator.java
@@ -1,32 +1,48 @@
 package requests;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.net.NetworkInterface;
 import java.net.SocketException;
-//import java.io.File;
 import java.util.UUID;
 
-public class IDCreater {
-    //TODO: UUID, MAC Adresse
-    //https://github.com/OOP-Regensburg/PluginHelper/blob/3329a75df571797d6a67c19821d2f36fb0d085cd/src/de/ur/mi/pluginhelper/logger/Log.java#L89
+//import java.io.File;
 
+public class IDCreator {
 
-    String id = UUID.randomUUID().toString();
-    private String createUUID() {
-        String uuid = UUID.randomUUID().toString();
-        System.out.println("Random UUID String = " + uuid);
-        return uuid;
+    //Muss das void bleiben?
+    public void createRequestID() {
+        //TODO: UUID, MAC Adresse
+
+        //Hier ID aus uuid und MAC Adresse zusammen bauen
+        String uuid = createUUID();
+        String hardwareAddress = getHardwareAddress();
+        String seperator = " - ";
+        String requestId = uuid + seperator + hardwareAddress;
+        System.out.println(requestId);
 
     }
 
+    @NotNull
+    private String createUUID() {
+        String uuid = UUID.randomUUID().toString();
+        return uuid;
+
+    }
+    //https://github.com/OOP-Regensburg/PluginHelper/blob/3329a75df571797d6a67c19821d2f36fb0d085cd/src/de/ur/mi/pluginhelper/logger/Log.java#L89
     //Methode übernommen von Alex
+
+    @NotNull
     private String getHardwareAddress() {
+        //return "Hardware Adresse";
         try {
-            byte[] address = NetworkInterface.getNetworkInterfaces().nextElement().getHardwareAddress();
-            StringBuilder addressBuilder = new StringBuilder();
-            for (int i = 0; i < address.length; i++) {
+           byte[] address = NetworkInterface.getNetworkInterfaces().nextElement().getHardwareAddress();
+          /* StringBuilder addressBuilder = new StringBuilder();
+           //Hier ist der Fehler!! address ist null
+           for (int i  = 0; i < address.length; i++) {
                 addressBuilder.append(String.format("%02X%s", address[i], (i < address.length - 1) ? "-" : ""));
             }
-            return addressBuilder.toString();
+            return addressBuilder.toString();*/
         } catch (SocketException e) {
             e.printStackTrace();
         }
