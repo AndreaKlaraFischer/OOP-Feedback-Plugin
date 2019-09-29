5e137d80fc264d361a2ac3ed79aaeea178c92c5a
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-09-29 16:32:47 +0200

    Created class "GitModel", moved cloneRepo method there.

--- Plugin/src/main/java/controller/Controller.java
+++ Plugin/src/main/java/controller/Controller.java
@@ -1,8 +1,10 @@
+
 package controller;
 
 import com.intellij.openapi.project.Project;
-import requests.StudentRequestModel;
+import requests.GitModel;
 import requests.IDCreator;
+import requests.StudentRequestModel;
 
 //zentrale Startklasse
 //beim Ausführen der Entwicklungsumgebung muss das Repo geklont werden
@@ -10,13 +12,16 @@ public class Controller {
     private Project project;
     private StudentRequestModel studentRequestModel;
     private IDCreator idCreator;
+    private GitModel gitModel;
     public Controller(Project project) {
         studentRequestModel = new StudentRequestModel(project);
         idCreator = new IDCreator();
+        gitModel = new GitModel(project);
         this.project = project;
         System.out.println("Startklasse funktioniert!");
         System.out.println(project.getBasePath());
-        studentRequestModel.cloneRepo();
+        gitModel.cloneRepo();
+        //Das steht hier nur zu Testzwecken
         idCreator.createRequestID();
     }
 
--- Plugin/src/main/java/requests/StudentRequestModel.java
+++ Plugin/src/main/java/requests/StudentRequestModel.java
@@ -1,56 +1,34 @@
 package requests;
 
 import com.intellij.openapi.project.Project;
+import config.Constants;
 import gui.SendRequestScreen;
 import org.eclipse.jgit.api.CheckoutCommand;
 import org.eclipse.jgit.api.CreateBranchCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.PushCommand;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
-import org.kohsuke.github.*;
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 
-import java.io.File;
 import java.io.IOException;
-
 
 
 //This class handles the requests from Plugin to GitHub
 public class StudentRequestModel {
-    private static final String REPO_URL = "https://github.com/OOP-Feedback/OOP-Feedback.git";
-    private static final String CLONED_REPO_FOLDER ="/ClonedRepo";
-    private static final String REPO_LOGIN = "OOP-Feedback";
-    private static final String REPO_PASSWORD = "FredFeedback1920";
     private SendRequestScreen sendRequestScreen;
     private IDCreator idCreator;
-    private File repoPath;
+   //private File repoPath;
 
     //Konstruktor
     public StudentRequestModel(Project project) {
         this.idCreator = new IDCreator();
-        this.sendRequestScreen = new SendRequestScreen(project);
-        String clonedRepoPath = project.getBasePath() + CLONED_REPO_FOLDER;
-        repoPath = new File(clonedRepoPath);
+        //this.sendRequestScreen = new SendRequestScreen(project);
+        /*String clonedRepoPath = project.getBasePath() + Constants.CLONED_REPO_FOLDER;
+        repoPath = new File(clonedRepoPath);*/
     }
 
-
-   /* However the destination location is chosen, explicitly through your code or by JGit,
-   the designated directory must either be empty or must not exist.
-   Otherwise, an exception will be thrown.*/
-    public void cloneRepo() {
-        try {
-            System.out.println("Repo Methodenaufruf funktioniert!");
-            Git.cloneRepository()
-                    .setURI(REPO_URL)
-                    .setCredentialsProvider( new UsernamePasswordCredentialsProvider( REPO_LOGIN, REPO_PASSWORD ) )
-                    .setDirectory(repoPath)
-                    .call();
-            System.out.println("Repo wurde erfolgreich geklont");
-        } catch (Exception e){
-            System.out.println("Repo wurde nicht erfolgreich geklont" + e.toString());
-        } finally {
-            //TODO: Ordner schließen/löschen
-        }
-    }
 
     //Übernommen von diesem Codebeispiel
     //https:stackoverflow.com/questions/33168230/jgit-create-new-local-branch-and-push-to-remote-branch-does-not-exist-on-remote
@@ -59,7 +37,7 @@ public class StudentRequestModel {
         System.out.println("Branchname" + branchName);
         try {
             // create branch
-            Git git = Git.open(repoPath);
+            Git git = Git.open(GitModel.repoPath);
             CreateBranchCommand createBranchCommand = git.branchCreate();
             createBranchCommand.setName(branchName);
             createBranchCommand.call();
@@ -71,7 +49,7 @@ public class StudentRequestModel {
 
             // push to remote
             PushCommand pushCommand = git.push();
-            pushCommand.setCredentialsProvider( new UsernamePasswordCredentialsProvider( REPO_LOGIN, REPO_PASSWORD ));
+            pushCommand.setCredentialsProvider( new UsernamePasswordCredentialsProvider( Constants.REPO_LOGIN, Constants.REPO_PASSWORD ));
             pushCommand.call();
             System.out.println("puschi");
 
@@ -83,14 +61,15 @@ public class StudentRequestModel {
     private void createIssue() {
         try {
             //https://github-api.kohsuke.org/apidocs/org/kohsuke/github/GitHub.html
-            GitHub github = GitHub.connectUsingPassword(REPO_LOGIN, REPO_PASSWORD);
-            GHRepository repo = github.getRepository("OOP-Feedback/OOP-Feedback");
+            GitHub github = GitHub.connectUsingPassword(Constants.REPO_LOGIN, Constants.REPO_PASSWORD);
+            GHRepository repo = github.getRepository(Constants.REPO_NAME);
             //TODO: Namen dynamisch erstellen
+            //TODO: Überlegen: Wie setzt sich der Titel zusammen? Soll man noch ein Betrefffeld einbauen oder werden die durchnummeriert? Zum Beispiel mit einem Counter?
             GHIssue issue = repo.createIssue("TITEL3").create();
-            //TODO: Body dynamisch erstellen
-            issue.setBody("TEST");
-            //TODO: Labels dynamisch erstellen
-            issue.addLabels("Hans", "Wurscht");
+            String issueBody = SendRequestScreen.inputMessage;
+            issue.setBody(issueBody);
+            String issueLabel = SendRequestScreen.problemCategory;
+            issue.addLabels(issueLabel);
         } catch (IOException e) {
             System.out.println("excepti + " + e.toString());
         }
