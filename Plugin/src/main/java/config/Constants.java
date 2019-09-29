45d88b2cde466e8ded8629a637a42130e54cc672
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-09-29 16:34:11 +0200

    Changed access in constant class to public.

--- Plugin/src/main/java/config/Constants.java
+++ Plugin/src/main/java/config/Constants.java
@@ -1,7 +1,9 @@
+package config;
+
 public class Constants {
-    private static final String REPO_URL = "https://github.com/OOP-Feedback/OOP-Feedback.git";
-    private static final String REPO_NAME = "OOP-Feedback/OOP-Feedback";
-    private static final String CLONED_REPO_FOLDER ="/ClonedRepo";
-    private static final String REPO_LOGIN = "OOP-Feedback";
-    private static final String REPO_PASSWORD = "FredFeedback1920";
+    public static final String REPO_URL = "https://github.com/OOP-Feedback/OOP-Feedback.git";
+    public static final String REPO_NAME = "OOP-Feedback/OOP-Feedback";
+    public static final String CLONED_REPO_FOLDER ="/ClonedRepo";
+    public static final String REPO_LOGIN = "OOP-Feedback";
+    public static final String REPO_PASSWORD = "FredFeedback1920";
 }
--- Plugin/src/main/java/requests/GitModel.java
+++ Plugin/src/main/java/requests/GitModel.java
@@ -1,4 +1,38 @@
 package requests;
 
+
+import com.intellij.openapi.project.Project;
+import config.Constants;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+
+import java.io.File;
+
 public class GitModel {
+    public static File repoPath;
+
+    public GitModel(Project project) {
+        String clonedRepoPath = project.getBasePath() + Constants.CLONED_REPO_FOLDER;
+        repoPath = new File(clonedRepoPath);
+    }
+
+    //TODO: Abfragen, ob Ordner schon existiert
+    /* However the destination location is chosen, explicitly through your code or by JGit,
+    the designated directory must either be empty or must not exist.
+    Otherwise, an exception will be thrown.*/
+    public void cloneRepo() {
+        try {
+            System.out.println("Repo Methodenaufruf funktioniert!");
+            Git.cloneRepository()
+                    .setURI(Constants.REPO_URL)
+                    .setCredentialsProvider( new UsernamePasswordCredentialsProvider(Constants.REPO_LOGIN, Constants.REPO_PASSWORD ) )
+                    .setDirectory(repoPath)
+                    .call();
+            System.out.println("Repo wurde erfolgreich geklont");
+        } catch (Exception e){
+            System.out.println("Repo wurde nicht erfolgreich geklont" + e.toString());
+        } finally {
+            //TODO: Ordner schließen/löschen
+        }
+    }
 }
