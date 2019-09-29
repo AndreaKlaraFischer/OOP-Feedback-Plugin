cc04c81c61a88d63ebf46c5c1e5226e44e4196e2
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-09-29 16:30:15 +0200

    Sourced constants out to seperate class

---
+++ Plugin/src/main/java/config/Constants.java
@@ -0,0 +1,7 @@
+public class Constants {
+    private static final String REPO_URL = "https://github.com/OOP-Feedback/OOP-Feedback.git";
+    private static final String REPO_NAME = "OOP-Feedback/OOP-Feedback";
+    private static final String CLONED_REPO_FOLDER ="/ClonedRepo";
+    private static final String REPO_LOGIN = "OOP-Feedback";
+    private static final String REPO_PASSWORD = "FredFeedback1920";
+}
