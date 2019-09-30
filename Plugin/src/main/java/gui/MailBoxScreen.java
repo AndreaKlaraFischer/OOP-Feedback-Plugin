a498365c03b16145e73f9eb49415536b467a5128
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-09-30 08:53:48 +0200

    Created class "Answer" for ArrayList for MailBoxScreen

--- Plugin/src/main/java/gui/MailBoxScreen.java
+++ Plugin/src/main/java/gui/MailBoxScreen.java
@@ -1,21 +1,46 @@
 package gui;
 
+import com.intellij.ui.components.JBList;
+import requests.Answer;
+import requests.GitHubModel;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class MailBoxScreen implements ActionListener {
     //TODO: Button für Actionlistener, Debugzwecke
+    private GitHubModel gitHubModel;
     private JPanel mailBoxScreenContent;
     private JButton testButton;
-    private JList list1;
-
+    private JBList answerList;
 
     public MailBoxScreen() {
+        this.gitHubModel = new GitHubModel();
         testButton.addActionListener(this::actionPerformed);
-
     }
+    //TODO: Liste dynamisch erstellen
+    //For-Schleife? //Array-List?
+    //if answers.size() > 0
+    //Wenn neue Antwort kam, dann soll der Liste etwas hinzugefügt werden (add()).
+   /* https://www.comp.nus.edu.sg/~cs3283/ftp/Java/swingConnect/tech_topics/jlist_1/jlist.html
+   In the following code snippet, we create a JList that displays the strings in a data[] array:
+
+
+    //String[] data = {"one", "two", "free", "four"};
+   //JList dataList = new JList(data);*/
+    //TODO: ArrayList erstellen
     //TODO: Wenn hier etwas geklickt wird, soll der "AnswerDetailScreen" aufgehen
+    //Wo muss diese Methode aufgerufen werden? Im Controller?
+    public void createAnswersList() {
+        ArrayList<Answer> answers = new ArrayList<Answer>();
+        Answer answer = new Answer();
+        //TODO: Hier soll das zusammen gebaut werden! Schleife? Bedingung?
+        //Immer, wenn eine passende Antwort gefunden wurde, soll geadded werden (GitHubModel)
+        answers.add(answer);
+        answerList = new JBList((ListModel) answers);
+    }
 
     public JPanel getContent() {
         return mailBoxScreenContent;
@@ -24,6 +49,7 @@ public class MailBoxScreen implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         System.out.println("Hier sollte dann etwas cooles passieren!");
+        gitHubModel.getClosedIssueList();
     }
 
 }
