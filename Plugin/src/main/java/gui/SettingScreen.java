351b02791e7b9fe009033d1311d72e5f051bba8d
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-11-09 14:57:55 +0100

    Added RegEx for email validation

--- Plugin/src/main/java/gui/SettingScreen.java
+++ Plugin/src/main/java/gui/SettingScreen.java
@@ -10,6 +10,8 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static javax.swing.JOptionPane.showMessageDialog;
 
@@ -19,10 +21,10 @@ public class SettingScreen implements ActionListener {
     private JButton generateAnonymousNameButton;
     private JButton saveSettingsButton;
     public JTextField inputMailAddressField;
-    public String studentMailInput;
     public static String studentNameInput;
     private Controller controller;
     private BalloonPopup balloonPopup;
+
 
     //07.10. --> Anmelden. Beim Start des Plugins StartScreen: Neuer Benutzer / Bestehender Benutzer
     // Kann man daheim wieder eingeben, wenn man es rechnerübergreifend benutzen möchte
@@ -40,7 +42,7 @@ public class SettingScreen implements ActionListener {
 
     //TODO: Florin fragen
     private String generateAnonymousName(ActionEvent actionEvent) {
-        String anonymousName ="";
+        String anonymousName = "";
         /*TODO hier: Jeweils ein Array aus n beliebigen Vornamen und Nachnamen basteln,
         jeweils zufällig einen auswählen und in das Textfeld eintragen. Prio Low
         */
@@ -55,26 +57,47 @@ public class SettingScreen implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(inputNameField.getText().length() == 0) {
+        if (inputNameField.getText().length() == 0) {
             showNoNameError();
-        //TODO: hier noch ein RegEx für validen MailInput !! Nice to have aber sehr praktisch
         } else if (inputMailAddressField.getText().length() == 0) {
             //TODO: Das muss noch irgendwie wegklickbar sein oder so, dass man es auch nach der Warnung noch abschicken kann.
             showNoMailWarning();
         } else {
-            try {
-                controller.onSaveSettingsButtonPressed();
-            } catch (IOException ex) {
-                ex.printStackTrace();
+            if (validateMail(inputMailAddressField.getText())) {
+                try {
+                    controller.onSaveSettingsButtonPressed();
+                    showSuccessfullySavedInfo();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            } else {
+                showInvalidMailError();
             }
         }
     }
 
-    public void showNoMailWarning() {
+
+    //https://stackoverflow.com/questions/8204680/java-regex-email
+    public boolean validateMail(String mailAddressInput) {
+        String emailRegex = "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+        Pattern emailPattern = Pattern.compile(emailRegex, Pattern.CASE_INSENSITIVE);
+        Matcher emailMatcher = emailPattern.matcher(mailAddressInput);
+        return emailMatcher.find();
+    }
+
+    private void showSuccessfullySavedInfo() {
+        balloonPopup.createBalloonPopup(settingScreenContent, Balloon.Position.above, "Erfolgreich gespeichert.", MessageType.INFO);
+    }
+
+    private void showInvalidMailError() {
+        balloonPopup.createBalloonPopup(settingScreenContent, Balloon.Position.above, "Ungültige Email-Adresse!", MessageType.ERROR);
+    }
+
+    private void showNoMailWarning() {
         balloonPopup.createBalloonPopup(settingScreenContent, Balloon.Position.above, "Wenn du keine Mailadresse angibst, bekommst du die Antwort des Tutors nicht zusätzlich per Mail zugeschickt.", MessageType.WARNING);
     }
 
-    public void showNoNameError() {
+    private void showNoNameError() {
         balloonPopup.createBalloonPopup(settingScreenContent, Balloon.Position.above, "Bitte einen Namen eingeben!.", MessageType.ERROR);
     }
 }
