fbb583cbab85f4695aa86f466e214cf26c3b0ff8
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-11-11 16:23:53 +0100

    Implemented random name generator

--- Plugin/src/main/java/requests/NameGenerator.java
+++ Plugin/src/main/java/requests/NameGenerator.java
@@ -1,4 +1,24 @@
 package requests;
 
+import com.github.javafaker.Faker;
+import controller.Controller;
+
 public class NameGenerator {
+    private Controller controller;
+
+    public NameGenerator(Controller controller) {
+        this.controller = controller;
+    }
+
+    //https://rieckpil.de/howto-generate-random-data-in-java-using-java-faker/
+    public void generateAnonymousName() {
+        String anonymousName = "";
+        //TODO: Branchname muss dann natürlich auch angepasst werden, damit man dort nicht den Namen rausfinden kann
+        Faker faker = new Faker();
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        anonymousName = "Anonyme/r " + firstName + " " + lastName;
+        System.out.println("anonymousName: " + anonymousName);
+        controller.settingScreen.inputNameField.setText(anonymousName);
+    }
 }
