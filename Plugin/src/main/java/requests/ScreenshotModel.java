2c7bddf899a2ac6f8c8f27c8502bac5774afc1ef
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-11-21 15:54:13 +0100

    Feedback for attached screenshots
    When the user selects a screenshot, a balloon with info message is visible as well as a list with the names of all attached screenshots.

--- Plugin/src/main/java/requests/ScreenshotModel.java
+++ Plugin/src/main/java/requests/ScreenshotModel.java
@@ -1,6 +1,5 @@
 package requests;
 
-import communication.GitModel;
 import config.Constants;
 import controller.Controller;
 import org.apache.commons.io.FileUtils;
@@ -9,23 +8,21 @@ import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ScreenshotModel {
 
-    public static File[] screenshots;
-    public File screenshotFolder;
+    private File[] screenshots;
+    private File screenshotFolder;
     public Controller controller;
-    public String allImagesString;
-    public String screenshotLabel;
-    public boolean screenshotAttached;
+    private String allImagesString;
+    private boolean screenshotAttached;
+    private ArrayList<String> screenshotTitles;
 
-    /*public ScreenshotModel(StudentRequestModel studentRequestModel) {
-        this.studentRequestModel = studentRequestModel;
-    }*/
     public ScreenshotModel(Controller controller) {
         this.controller = controller;
-        //gitModel = controller.gitModel;
+        screenshotTitles = new ArrayList<>();
     }
 
     public void chooseFiles() {
@@ -42,54 +39,49 @@ public class ScreenshotModel {
             if (returnValue == JFileChooser.APPROVE_OPTION) {
                 screenshots = screenshotUploader.getSelectedFiles();
                 System.out.println("Files Found\n");
-                //TODO: Hier komprimieren?
                 Arrays.asList(screenshots).forEach(file -> {
                     if (file.isFile()) {
+                        screenshotTitles.add(file.getName());
                         System.out.println(file.getName());
                     }
                 });
             }
             //Wenn es Screenshots gibt, dann Ordner erstellen und Screenshots hinzufügen
-
             if (screenshots.length > 0) {
-               hasScreenShotAttached();
-                screenshotLabel = "screenshot angehängt";
+                screenshotAttached = true;
+                addScreenshotTitlesToLabel();
                 addScreenshotsToBranch();
                 addScreenshotsToIssue(screenshots);
-            } else {
-                screenshotLabel = "";
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //TODO: Hier Balloon machen
+        controller.sendRequestScreen.showScreenshotAttachedInfo();
+    }
 
+    private void addScreenshotTitlesToLabel() {
+        controller.sendRequestScreen.attachedScreenshotsLabel.setText(String.valueOf(controller.screenshotModel.screenshotTitles));
+        controller.sendRequestScreen.attachedScreenshotsLabel.setVisible(true);
     }
 
     public boolean hasScreenShotAttached() {
-        screenshotAttached = true;
-        return true;
-    }
-
-    public String getScreenshotLabel() {
-        return screenshotLabel;
+        return screenshotAttached;
     }
 
     //Hier das Bild zusammenwurschteln und das dann dem Issue mitgeben! --> Im Controller!
-    public String addScreenshotsToIssue(File [] screenshots) {
+    private String addScreenshotsToIssue(File[] screenshots) {
         String imageUrl = ""; // Das wird spannend
         String image;
-        if(screenshots.length > 0) {
-            for (int i = 0; i < screenshots.length; i++) {
-                File screenshot = screenshots[i];
+        if (screenshots.length > 0) {
+            for (File screenshot : screenshots) {
                 String fileName = screenshot.getName();
                 String imageName = fileName.substring(0, fileName.lastIndexOf('.'));
                 System.out.println("Imagename: " + imageName);
-                //TODO: Das vllt ins GitModel auslagern um zu tricksen?
-                String linkToImage = controller.gitModel.createImageUrl();
+                //TODO: ?
+                String linkToImage = createImageUrl();
                 System.out.println(linkToImage);
                 //String fileExtension = fileName.substring(fileName.lastIndexOf('.'));
-               // System.out.println("Endung: " + fileExtension);
+                // System.out.println("Endung: " + fileExtension);
                 //https://github.com/OOP-Feedback-Test/OOP-Feedback/blob/branch-zdt-2-2810-2107/screenshots/testBild.jpg
                 //imageUrl = "https://user-images.githubusercontent.com/" + linkToImage + fileName;
                 imageUrl = "https://raw.githubusercontent.com/" + linkToImage + fileName;
@@ -101,33 +93,38 @@ public class ScreenshotModel {
         return allImagesString;
     }
 
+    public String createImageUrl() {
+        return "OOP-Feedback-Test/OOP-Feedback/blob/" + controller.getBranchName() + "/screenshots/";
+    }
+
     public String getAllImagesString() {
         return allImagesString;
     }
 
-    public void addScreenshotsToBranch()  {
-       screenshotFolder = new File( controller.gitModel.projectPath + Constants.SCREENSHOT_FOLDER);
+    private void addScreenshotsToBranch() {
+        //TODO: Warum Umweg über das gitmodel?- Refactor
+        String projectPath = controller.project.getBasePath();
+        screenshotFolder = new File(projectPath + Constants.SCREENSHOT_FOLDER);
         screenshotFolder.mkdir();
 
         for (File file : screenshots) {
             System.out.println(controller.gitModel.projectPath + Constants.SCREENSHOT_FOLDER + "/" + file.getName());
-            File screenshot = new File(controller.gitModel.projectPath + Constants.SCREENSHOT_FOLDER + "/" + file.getName());
+            File screenshot = new File(projectPath + Constants.SCREENSHOT_FOLDER + "/" + file.getName());
             try {
                 FileUtils.copyFile(file, screenshot);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            System.out.println("Schwurbel" + screenshot);
         }
     }
 
-
-    public void deleteScreenshotFolder() {
-        try {
-            FileUtils.cleanDirectory(screenshotFolder);
-            FileUtils.deleteDirectory(screenshotFolder);
-        } catch (Exception e) {
-            System.out.println("Ordner löschen " + e.toString());
-        }
+    public void clearScreenshotFolder() {
+       // if (screenshots.length == 0) {
+            try {
+                FileUtils.cleanDirectory(screenshotFolder);
+            } catch (Exception e) {
+                System.out.println("Ordner löschen " + e.toString());
+            }
+       // }
     }
 }
