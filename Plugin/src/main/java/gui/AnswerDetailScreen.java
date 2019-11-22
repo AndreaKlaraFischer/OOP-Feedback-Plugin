257e225249a8f90a0426a93de051afaaa01c0c75
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-11-22 15:41:33 +0100

    Implemented actionListener for each image in in imageIconList -> On click on an attached screenshot, a JFrame gets opened with the corresponding image in higher resolution.

--- Plugin/src/main/java/gui/AnswerDetailScreen.java
+++ Plugin/src/main/java/gui/AnswerDetailScreen.java
@@ -1,32 +1,25 @@
 package gui;
 
-import actions.BalloonPopup;
 import answers.ImagePanel;
 import com.intellij.openapi.ui.MessageType;
 import com.intellij.openapi.ui.popup.Balloon;
 import com.intellij.ui.components.JBScrollPane;
 import config.Constants;
 import controller.Controller;
-import org.apache.batik.ext.awt.image.spi.ImageWriterParams;
+import org.eclipse.jgit.api.errors.GitAPIException;
 
 import javax.imageio.ImageIO;
-import javax.imageio.ImageWriteParam;
-import javax.imageio.ImageWriter;
-import javax.imageio.stream.ImageOutputStream;
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.URL;
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
 
-//TODO: Das wird wahrscheinlich auch nur so lang funktionieren, bis die .form files wieder gehen
 public class AnswerDetailScreen  implements ActionListener {
     private JPanel testPanel;
     private MailBoxScreen mailBoxScreen;
@@ -52,12 +45,16 @@ public class AnswerDetailScreen  implements ActionListener {
     //private JButton imageButton;
 
     private JFrame highResolutionFrame;
+    //17.11.
+    public ArrayList<JButton> imageButtonList;
 
 
     public AnswerDetailScreen(MailBoxScreen mailBoxScreen, Controller controller) {
         this.controller = controller;
         this.mailBoxScreen = mailBoxScreen;
         controller.answerDetailScreen = this;
+        //17.11.
+        imageButtonList = new ArrayList<>();
 
         GridLayout testLayout = new GridLayout(3,2);
         detailedAnswerField = new JTextField();
@@ -69,7 +66,7 @@ public class AnswerDetailScreen  implements ActionListener {
 
         JButton openCodeButton = new JButton();
         openCodeButton.setText("zum Code");
-        openCodeButton.addActionListener(this::openCodeInNewWindow);
+        openCodeButton.addActionListener(this);
         openCodeButton.setVisible(false);
         sendFeedbackButton.addActionListener(this);
         JButton backButton = new JButton();
@@ -103,62 +100,61 @@ public class AnswerDetailScreen  implements ActionListener {
         //24.10. Test
        // answerDetailPanel.add(imageLabel);
 
-
+        //TODO: Wenn Anfragen abgeschickt wurden und noch nicht beantwortet sind, dann anzeigen! Liste mit offenen Anfragen (Visability of system state)
 
 
         answerDetailScrollPane = new JBScrollPane(answerDetailPanel);
     }
 
-    //TODO: Bilder komprimieren
-    public  Image createImageFromAttachedImageFile(List<String> imageUrls) {
-        //24.10. Test mit hardgecodeter URL, funktioniert
-        Image image = null;
+    public void createImageFromAttachedImageFile(List<String> imageUrls) {
         try {
-
-            for(int i = 0; i < imageUrls.size(); i++) {
-                //TODO: Das muss ich übergeben
-                BufferedImage img = ImageIO.read(new URL(
-                        //TODO: Hier den Link, den ich aus dem Bildkommentar geholt habe
-                        imageUrls.get(i)));
+            for (String imageUrl : imageUrls) {
+                BufferedImage img = ImageIO.read(new URL(imageUrl));
                 //"http://www.java2s.com/style/download.png"));
                 ImageIcon icon = new ImageIcon(img);
                 //Bild transformieren
-                image = icon.getImage();
-                Image scaledImage = image.getScaledInstance(120,120, Image.SCALE_SMOOTH);
+                Image image = icon.getImage();
+                Image scaledImage = image.getScaledInstance(120, 120, Image.SCALE_SMOOTH);
                 icon = new ImageIcon(scaledImage);
                 //Bild dem Button hinzufügen
                 JButton imageButton = new JButton(icon);
-                imageButton.addActionListener(this::openImageHighResolution);
+                //17.11.
+                imageButtonList.add(imageButton);
+                imageButton.addActionListener(e -> {
+                    openImageHighResolution(img);
+                });
                 answerDetailPanel.add(imageButton);
 
                 //Versuch
-                addImageToFrame(image);
-
+                //addImageToFrame(image);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return image;
     }
 
-
-    private void addImageToFrame(Image image) {
-        highResolutionFrame = new JFrame();
-        highResolutionFrame.setContentPane(new ImagePanel(image));
-    }
-    //TODO: Bild übergeben. Das geht noch nicht so.
-    private void openImageHighResolution(ActionEvent e) {
+    private void openImageHighResolution(BufferedImage img) {
       System.out.println("Wird aufgerufen und ich weiß noch nicht so genau, wie ich den Frame erzeugen lassen soll");
-      highResolutionFrame.setSize(560,500);
-      highResolutionFrame.pack();
-      highResolutionFrame.setVisible(true);
-    }
 
+        JFrame f = new JFrame("Image pane");
+        f.setSize(img.getWidth() / 2, img.getHeight() / 2);
+        Image scaledImg = img.getScaledInstance(img.getWidth() / 2, img.getHeight() / 2, Image.SCALE_SMOOTH);
+        JBScrollPane scrolli = new JBScrollPane(new ImagePanel(scaledImg));
+        //f.setContentPane(scrolli);
+        f.getContentPane().add(scrolli);
+        //f.pack();
+        f.setVisible(true);
+    }
 
     //TODO: Hier dann das Fenster öffnen, was aktuell noch im Tutorialscreen aufgerufen wird
-    private void openCodeInNewWindow(ActionEvent e) {
-        controller.onOpenCodeButtonPressed();
+    //TODO: Controller
+    private void openCodeInNewWindow() {
         System.out.println("Open annotated Code");
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     //TODO: Das geht nicht! Das funktioniert nicht mit dem TutorialScreen
@@ -166,22 +162,24 @@ public class AnswerDetailScreen  implements ActionListener {
     //--> Sorgt sonst nur für Verwirrung
     //10.11. Ausgabe wird aufgerufen, aber der Button wird nicht manipuliert. Vielleicht sollte ich das umgekehrt machen
     //--> Standardmäßig nicht zeigen, aber wenn Änderungen da sind, enablen
-    public void activateOpenCodeButton() {
+    public void activateOpenCodeButton() throws GitAPIException, IOException, BadLocationException {
+        if(controller.getDiffEntries().size() > 0) {
+            controller.hasChanges = true;
+        }
         System.out.println(controller.hasChanges);
         if(controller.hasChanges) {
             openCodeButton.setVisible(true);
             openCodeButton.setEnabled(true);
-        } else {
+        } /*else {
             System.out.println("keine Änderungen!");
             openCodeButton.setVisible(false);
             openCodeButton.setEnabled(false);
-        }
+        }*/
     }
 
     private void backToTable(ActionEvent e) {
         mailBoxScreen.navigateBackToTable();
     }
-
 
     private Object getClickedButton(ActionEvent e) {
         return e.getSource();
@@ -201,13 +199,12 @@ public class AnswerDetailScreen  implements ActionListener {
         } else if (clickedButton == sendFeedbackButton) {
             controller.sendFeedbackForFeedback();
         } else if(clickedButton == solvedCheckbox) {
-            //TODO: Hier Label anhängen
             taskSuccessfullySolved = Constants.PROBLEM_SOLVED_SUCCESSFULLY;
             controller.sendProblemSolved();
+        } else if(clickedButton == openCodeButton) {
+            openCodeInNewWindow();
         }
     }
-
-
 
     public void createFeedbackText() {
         String prefix = Constants.GITHUB_BOLD_AND_ITALIC + Constants.FEEDBACK_FOR_FEEDBACK + Constants.GITHUB_BOLD_AND_ITALIC;
