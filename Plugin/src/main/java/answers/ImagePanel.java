10eab3e62c24fc78d29d180e88639fac71249f9d
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-11-22 15:34:10 +0100

    Implemented ImagePanel class for displaying attached screenshots in high resolution in frame

--- Plugin/src/main/java/answers/ImagePanel.java
+++ Plugin/src/main/java/answers/ImagePanel.java
@@ -12,17 +12,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 //https://stackoverflow.com/questions/1064977/setting-background-images-in-jframe
-public class ImagePanel extends Container {
-    //15.11. Das zu Testzwecken.
-  private Image image;
-  public ImagePanel(Image image) {
-      this.image = image;
-  }
-
-  protected void paintComponent() {
-
-  }
-
-
-
+public class ImagePanel extends JComponent {
+    private Image image;
+    public ImagePanel(Image image) {
+        this.image = image;
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this);
+    }
 }
