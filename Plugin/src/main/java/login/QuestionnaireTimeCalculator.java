302e19c146c269d85b344857d4e59676f8a9a1aa
Andrea Fischer <andrea.fischer@stud.uni-regensburg.de>
2019-12-07 22:48:47 +0100

    Implemented basic (hard coded) time calculator for between questionnaires by comparing dates

--- Plugin/src/main/java/login/QuestionnaireTimeCalculator.java
+++ Plugin/src/main/java/login/QuestionnaireTimeCalculator.java
@@ -18,31 +18,39 @@ public class QuestionnaireTimeCalculator {
 
     //https://stackoverflow.com/questions/12087419/adding-days-to-a-date-in-java
     public boolean calculateQuestionnaireDelta() throws ParseException {
-        //TODO: Das noch richtig machen!
         boolean isTimeForQuestionnaire = false;
 
         String savedDate = controller.XMLFileReader.readDateFromXML();
         //String currentDateLong = controller.getCurrentDate();
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Constants.SIMPLE_DATE_FORMAT);
+
         Date currentTime = new Date();
-       String currentDateShort  =  simpleDateFormat.format(currentTime);
+
+       String currentDateShortString  =  simpleDateFormat.format(currentTime);
 
 
-       // String expectedQuestionnaireDate =
+
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(simpleDateFormat.parse(currentDateShort));
-        //calendar.add(Calendar.DATE, Constants.DAYS_BETWEEN_QUESTIONNAIRES);
-        calendar.add(Calendar.DATE, 3);
-        // Date expectedQuestionnaireDate = simpleDateFormat.parse(calendar.getTime().toString());
-        String expectedQuestionnaireDate = simpleDateFormat.format(calendar.getTime());
-        System.out.println("expectedTime nach dem addieren: " + expectedQuestionnaireDate);
+       // calendar.setTime(simpleDateFormat.parse(currentDateShortString));
+        calendar.setTime(simpleDateFormat.parse(savedDate));
+        System.out.println("Saved Date: " + savedDate);
+        calendar.add(Calendar.DATE, Constants.DAYS_BETWEEN_QUESTIONNAIRES);
+       String expectedQuestionnaireDateString = simpleDateFormat.format(calendar.getTime());
 
-        //TODO: Oder größer!! Testen, was passiert wenn "gestern" erwartet grwesen wäre (hier mit Dates arbeiten statt mit Strings)
-        if (expectedQuestionnaireDate.equals(currentDateShort))  {
+        System.out.println("expectedTime nach dem addieren: " + expectedQuestionnaireDateString);
+
+        //TODO: Das vielleicht noch ein bisschen schöner machen, damit man nicht so oft parst.
+        Date currentDateShort = simpleDateFormat.parse(currentDateShortString);
+        System.out.println("Date current nach parsen: " + currentDateShort);
+        Date expectedQuestionnaireDate = simpleDateFormat.parse(expectedQuestionnaireDateString);
+        System.out.println("Date expected nach parsen: " + expectedQuestionnaireDate);
+
+        //Wenn der Abstand 3 Tage ist oder größer
+        //Future Work: Das an die Tage anpassen, immer runterrechnen auf die Zeit, die noch übrig bleibt
+        if(currentDateShortString.compareTo(expectedQuestionnaireDateString) > 0 || currentDateShortString.compareTo(expectedQuestionnaireDateString) == 0) {
+            System.out.println(currentDateShortString + ">" +  expectedQuestionnaireDateString);
             isTimeForQuestionnaire = true;
-
-
         }
         return isTimeForQuestionnaire;
     }
